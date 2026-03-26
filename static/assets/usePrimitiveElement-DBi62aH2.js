b4e218580e84dc04d24556c33b15ca483e860c21
XiaoHuang <1670884979@qq.com>
2026-03-26 10:46:18 +0800

    build: update static assets

--- static/assets/usePrimitiveElement-DBi62aH2.js
+++
@@ -1,1 +0,0 @@
-import{c as n}from"./index-_Hk4b1iJ.js";import{k as m,r}from"./index-D6gXPpEp.js";function o(){const e=r(),t=m(()=>["#text","#comment"].includes(e.value?.$el.nodeName)?e.value?.$el.nextElementSibling:n(e));return{primitiveElement:e,currentElement:t}}export{o as u};
